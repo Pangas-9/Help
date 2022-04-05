2a11112a54395b8c1fa64d9f0591312417b5040e
Nick Wylynko <nick@blueskylabs.com.au>
2022-04-05 19:24:29 +0800

    first check its a fireball, then check it is a valid duration, the compare the duration

--- Again.py
+++ Again.py
@@ -1,28 +1,46 @@
 # your code here
 import csv
+
 sightings_us = []
 filepath = "ufo-sightings.csv"
+
 with open(filepath, "r") as csvfile:
-    reader = csv.DictReader(csvfile) 
+
+    reader = csv.DictReader(csvfile)
+
     sightings_us = [row for row in reader if row["country"] == "us"]
 
-#First, define a Python function that checks if a given duration (seconds) is "valid"
+# First, define a Python function that checks if a given duration (seconds) is "valid"
 
 
 def is_valid_duration(duration_as_string):
-# your code here
-   
+    # your code here
+
     try:
         duration = float(duration_as_string)
-        
+
     except ValueError:
         return False
-    
+
     else:
         return True
 
-is_valid_duration(sightings_us["duration (seconds)"])
-    
 
-fball = [row for row in sightings_us if row["shape"] == "fireball" and is_valid_duration(row["duration (seconds)"]) > 10]
-fball
+# is_valid_duration(sightings_us["duration (seconds)"])
+
+
+fball = []
+
+for row in sightings_us:
+
+    if row["shape"] == "fireball":
+
+        duration = row["duration (seconds)"]
+
+        if is_valid_duration(duration):
+
+            if float(duration) > 10:
+
+                fball.append(row)
+
+print(fball)
